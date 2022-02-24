a505160ba87469d595fcb9822f797b63092ca667
ryman1 <ryman1@gmail.com>
2022-02-24 12:20:05 -0700

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 import java.net.*;
 import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
         private Socket socket            = null;
@@ -12,7 +15,10 @@ public class Main {
 
             // establish a connection
             try {
-                socket = new Socket(address, port);
+                System.out.println(port);
+                socket = new Socket();
+                socket.connect(new InetSocketAddress(address, port), 1000);
+//                socket.setSoTimeout(3);
                 System.out.println("Connected");
 
                 // takes input from terminal
@@ -47,10 +53,24 @@ public class Main {
             } catch (IOException i) {
                 System.out.println(i);
             }
+            catch (NullPointerException n){
+                System.out.println("No connection");
+            }
         }
 
 
     public static void main(String[] args) {
         Main client = new Main("72.14.183.78", 22);
+        Main client1 = new Main("72.14.183.78", 443);
+        Main client2 = new Main("72.14.183.78", 80);
+        Main client3 = new Main("72.14.183.78", 8080);
+        Main client4 = new Main("72.14.183.78", 21);
+        Main client5 = new Main("72.14.183.78", 8000);
+        Main client6 = new Main("72.14.183.78", 8443);
+        Main client7 = new Main("72.14.183.78", 23);
+        Main client8 = new Main("72.14.183.78", 9000);
+        Main client9 = new Main("72.14.183.78", 10000);
+        Main client11 = new Main("72.14.183.78", 53);
+        Main client12 = new Main("72.14.183.78", 445);
     }
 }
